218249196c4b0dafd3fdcbf446e44852e4f4339c
Nino <Nino.Gorgiladze14@gmail.com>
2020-06-27 12:37:06 +0400

    Fixed an issue where portal was being drawn for an extra frame.

--- Engine/Portal.cpp
+++ Engine/Portal.cpp
@@ -13,7 +13,7 @@ Portal::Portal(const Vec2& pos, float FullyOpenDuration)
 
 void Portal::Draw(Graphics& gfx) const
 {
-	float t = FullyOpenDuration + 0.8f;
+	float t = FullyOpenDuration + 0.6f;
 		if (Time < t)
 		{
 			animations[(int)iCurSequence].Draw((Vei2)pos, gfx);
@@ -25,13 +25,11 @@ void Portal::Update(float dt)
 	Time += dt;
 	if (Time >= 1.2f)
 	{
-		//at this point the portal is fully open
 		FullyOpened = true;
 		iCurSequence = Sequence::FullyOpen;
 	}
 	if (Time >= FullyOpenDuration)
 	{
-		//at this point the portal needs to close
 		iCurSequence = Sequence::Closing;
 	}
 
